713e517580d391b5f1cec1241bbae8ab233f5bf2
ishi720 <ishizuka720@gmail.com>
2017-11-18 10:37:42 +0900

    imagineに書き換え

--- img_crop.php
+++ img_crop.php
@@ -1,31 +1,30 @@
 <?php
 
+require 'vendor/autoload.php';
+$imagine = new Imagine\Gd\Imagine();
+
 $filename = htmlspecialchars($_GET["img_url"],ENT_QUOTES, "UTF-8");
-$img = imagecreatefromjpeg($filename);
 
 //サイズの取得
-$img_w = imagesx($img);
-$img_h = imagesy($img);
+$img = $imagine->open($filename);
+$size = $img->getSize();
 
 // トリミングするサイズを決める(小さいサイズに合わせる)
-$min_size = min($img_w, $img_h); 
+$min_size = min( ($size->getWidth()) , ($size->getHeight()) ); 
 
 // トリミングする位置を決める(中央よせ)
-if ( $img_w >= $img_h ) { //横幅のほうが大きい場合
-    $x = $img_w/2 - $img_h/2;
+if ( $size->getWidth() >= $size->getHeight()) { //横幅のほうが大きい場合
+    $x = $size->getWidth()/2 - $size->getHeight()/2;
     $y = 0;
 } else { //縦幅のほうが大きい場合
     $x = 0;
-    $y = $img_h/2 - $img_w/2;
+    $y = $size->getHeight()/2 - $size->getWidth()/2;
 }
 
-//トリミングする
-$crop_img = imagecrop($img, ['x' => $x, 'y' => $y, 'width' => $min_size, 'height' => $min_size]);
+$box = new Imagine\Image\Box($min_size, $min_size);
+$point = new Imagine\Image\Point($x, $y);
 
-header("Content-Type: image/jpeg");
-
-//出力
-if ($crop_img !== FALSE) {
-    imagejpeg($crop_img);
-}
-exit;
+//トリミングして表示
+$imagine->open($filename)
+	->crop($point, $box)
+	->show('jpg');
